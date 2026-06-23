bd413d66420e76e0add97bdd8bcd61ba77b73de2
Leandro Gonzalez <leanlag@gmail.com>
2026-06-23 20:31:20 -0300

    fix: elimina comentario innecesario de paymentRouter

--- server/router/paymentRouter.js
+++ server/router/paymentRouter.js
@@ -2,10 +2,8 @@ const express = require("express");
 const PaymentController = require("../controllers/paymentController");
 const { authUser } = require("../middlewares/authUserMiddleware");
 
-//Creo el router
 const api = express.Router();
 
-//Rutas
 api.get("/mercadopagoapikey", PaymentController.getMercadoPagoApiKey);
 api.post("/payment", authUser, PaymentController.createPreference);
 api.post("/webhook", PaymentController.webhook);
